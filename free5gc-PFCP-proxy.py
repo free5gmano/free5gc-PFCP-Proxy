cf7507ed51a57927190e61eb94df0217fc9ffdf6
root <root@ch69-7.maas>
2023-03-21 09:48:06 +0000

    add mqtt

--- free5gc-PFCP-proxy.py
+++ free5gc-PFCP-proxy.py
@@ -1,24 +1,89 @@
-from scapy.all import *
+import argparse
+import signal
+import logging
+import select
+import socket
+from threading import Thread
+import paho.mqtt.client as mqtt
+from kubernetes import client, config, utils
+import time
+import os
 
-def main (x):
-    try:
-        pktParsed = Ether(str(x))
-        if pktParsed[UDP].dport != 8805:
-            return
-        else:
-            MACsrc = pktParsed[Ether].src
-            MACdst = pktParsed[Ether].dst
-            IPsrc = pktParsed[IP].src
-            IPdst = pktParsed[IP].dst
-            print(IPsrc)
-            print(IPdst)
-            TCPsrc = pktParsed[UDP].sport
-            TCPdst = pktParsed[UDP].dport
-            b = IP(src = IPdst, dst = IPsrc)
-            sendp(b, iface="eno1")
-    except:
-        return
+
+FORMAT = '%(asctime)-15s %(levelname)-10s %(message)s'
+logging.basicConfig(format=FORMAT)
+LOGGER = logging.getLogger()
+
+LOCAL_DATA_HANDLER = lambda x:x
+REMOTE_DATA_HANDLER = lambda x:x
+
+BUFFER_SIZE = 2 ** 10
+
+PFCP_DATA = None
+PFCP_RESENDING = False
+
+def pfcp_proxy(host, upfs):
+    proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    proxy_socket.bind(ip_to_tuple(host))
+
+    smf = None
+    upf_address = ip_to_tuple(upfs)
     
-if __name__ == "__main__":
-    sniff(iface="eno1", prn=lambda x: main(x))
+    while True:
+        data, address = proxy_socket.recvfrom(BUFFER_SIZE)
 
+        if smf == None:
+            smf = address
+
+        if address == smf:
+            data = LOCAL_DATA_HANDLER(data)
+            PFCP_DATA = data
+            print(PFCP_DATA)
+            proxy_socket.sendto(data, upf_address)         
+        elif address == upf_address and PFCP_RESENDING == False:
+            data = REMOTE_DATA_HANDLER(data)
+            proxy_socket.sendto(data, smf)
+            smf = None
+        elif address == upf_address and PFCP_RESENDING == True:
+            PFCP_RESENDING = False
+    
+def resend_pfcp(upf_ip):
+    proxy_socket.sendto(PFCP_DATA, upf_ip)
+    PFCP_RESENDING = True
+
+def ip_to_tuple(ip):
+    ip, port = ip.split(':')
+    return (ip, int(port))
+
+def main():
+    host = "10.20.1.57:8805"
+    upfs = "10.20.1.58:8805"
+    # pfcp_proxy(host, upfs)   
+    t = Thread(target=pfcp_proxy, args=(host, upfs))
+    t.start()
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.username_pw_set("try","xxxx")
+    client.connect("10.0.0.218", 1883, 60)
+    client.loop_forever()
+
+
+def on_message(client, userdata, msg):
+    print(client, userdata)
+    print(msg.topic+" "+ msg.payload.decode('utf-8'))
+    # if msg.payload.decode('utf-8')[msg.payload.decode('utf-8')]
+    resend_pfcp(msg.payload.decode('utf-8').split(":")[1])
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("upf/status")
+
+if __name__ == '__main__':
+    main()
+
+    
+
+    
---
+++ upf_moniter.py
@@ -0,0 +1,35 @@
+import paho.mqtt.client as mqtt
+import json
+import time
+from kubernetes import client, config, utils
+
+config.load_kube_config()
+v1 = client.CoreV1Api()
+k8s_client = client.ApiClient()
+k8s_api = client.ExtensionsV1beta1Api(k8s_client)
+
+client = mqtt.Client()
+client.username_pw_set("try","xxxx")
+client.connect("10.0.0.218", 1883, 60)
+
+def send_upf_err_msg():
+    payload = {'upf_status' : "upf_err_ip:10.20.1.58"}
+    print (json.dumps(payload))
+    client.publish("upf/status", json.dumps(payload))
+
+def restart_upf():
+    utils.create_from_yaml(k8s_client, "/home/ubuntu/3.2.1cni_nodeport/02-free5gc-upf.yaml")
+
+while True:
+    time.sleep(10)
+    ret = v1.list_namespaced_pod("default")
+    if len(ret.items) == 0:
+        # send_upf_err_msg()
+        restart_upf()
+        send_upf_err_msg()
+    else:
+        for i in ret.items:
+            if i.metadata.name.find("free5gc-upf") != -1:
+                if i.status.phase != "Running":
+                    send_upf_err_msg()
+                    restart_upf()
